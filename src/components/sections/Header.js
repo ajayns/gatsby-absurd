c92e14f36d044c0e8d655ab51536decfe8df5cb7
ajayns <ajay.ns@outlook.com>
2019-03-17 22:44:06 +0530

    :iphone: Responsiveness

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -47,6 +47,10 @@ const Header = () => (
 const HeaderWrapper = styled.header`
   background-color: ${props => props.theme.color.primary};
   padding-top: 96px;
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    padding-top: 128px;
+  }
 `;
 
 const Art = styled.figure`
