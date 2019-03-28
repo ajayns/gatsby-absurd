bde2241f1a98bbbe0fb80c36b879ed8832670ad7
ajayns <ajay.ns@outlook.com>
2019-03-28 13:56:13 +0530

    :pencil: Update FAQ copy

--- src/components/sections/FAQ.js
+++ src/components/sections/FAQ.js
@@ -7,32 +7,58 @@ import ExternalLink from '@common/ExternalLink';
 
 const FAQS = [
   {
-    title: 'What did Obi-Wan Kenobi say?',
+    title: 'What tech does Gatsby use?',
     content: () => (
       <>
-        If you define yourself by your power to take life, your desire to
-        dominate, to possess, then you have nothing.
+        Enjoy the power of the latest web technologies – React.js , Webpack ,
+        modern JavaScript and CSS and more — all set up and waiting for you to
+        start building
       </>
     ),
   },
   {
-    title: 'Jedi wisdom on love?',
+    title: 'Where can you source data from?',
     content: () => (
       <>
-        Attachment is forbidden. Possession is forbidden. Compassion, which I
-        would define as unconditional love, is essential to a Jedi’s life. So
-        you might say, that we are encouraged to love.
+        Gatsby’s rich data plugin ecosystem lets you build sites with the data
+        you want — from one or many sources: Pull data from headless CMSs, SaaS
+        services, APIs, databases, your file system, and more directly into your
+        pages using GraphQL.
       </>
     ),
   },
   {
-    title: 'Where did you even find these FAQs?',
+    title: 'How do I scale Gatsby sites?',
     content: () => (
       <>
-        Ripped straight from{' '}
-        <ExternalLink href="https://www.starwars.com/news/40-memorable-star-wars-quotes">
-          here
-        </ExternalLink>
+        Gatsby.js is Internet Scale. Forget complicated deploys with databases
+        and servers and their expensive, time-consuming setup costs,
+        maintenance, and scaling fears. Gatsby.js builds your site as “static”
+        files which can be deployed easily on dozens of services.
+      </>
+    ),
+  },
+  {
+    title: 'How does do I future-proof my website?',
+    content: () => (
+      <>
+        Do not build a website with last decade’s tech. The future of the web is
+        mobile, JavaScript and APIs—the{` `}
+        <ExternalLink href="https://jamstack.org/">JAMstack</ExternalLink>.
+        Every website is a web app and every web app is a website. Gatsby.js is
+        the universal JavaScript framework you’ve been waiting for.
+      </>
+    ),
+  },
+  {
+    title: 'What exactly does Gatsby build?',
+    content: () => (
+      <>
+        Gatsby.js is a static PWA (Progressive Web App) generator. You get code
+        and data splitting out-of-the-box. Gatsby loads only the critical HTML,
+        CSS, data, and JavaScript so your site loads as fast as possible. Once
+        loaded, Gatsby prefetches resources for other pages so clicking around
+        the site feels incredibly fast.
       </>
     ),
   },
