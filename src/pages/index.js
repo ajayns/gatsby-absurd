69978313f1da1684da2db0cd578ddfe05974954c
Vince Fulco <47701145+weisisheng@users.noreply.github.com>
2019-04-26 21:37:57 +0800

    Change file name reference so dev compiles

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Header from '@sections/Header';
 import About from '@sections/About';
 import Brands from '@sections/Brands';
 import Team from '@sections/Team';
-import Faq from '@sections/Faq';
+import Faq from '@sections/FAQ';
 import Footer from '@sections/Footer';
 
 const IndexPage = () => (
